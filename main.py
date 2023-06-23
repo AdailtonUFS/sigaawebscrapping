01e0051a328409f8faf3a0c39a925ad71fd04abc
Adailton Moura <contatoadailtonmoura@gmail.com>
2023-06-23 02:41:51 -0300

    classes requests payload

--- main.py
+++ main.py
@@ -15,12 +15,28 @@ session.post(loginRouter.url, data=login_data)
 
 menuDiscenteRouter = urls['menu_discente']
 menu_discente_request = session.get(menuDiscenteRouter.url)
-print(menu_discente_request.text)
-
-
 
 site = BeautifulSoup(menu_discente_request.text, "html.parser")
 
-print(site.find(id="turmas-portal"))
+turmas = site.find(id="turmas-portal")
+forms = turmas.find_all('form', id=lambda value: value and 'acessarTurmaVirtualj' in value)
 
+turmas_array = []
+for form in forms:
+    request_payload = {}
+    form_name = form.get("name")
+    request_payload['form_acessarTurmaVirtual'] = form_name
+
+    idTurma_input = form.find('input', attrs={'name': 'idTurma'})
+    if idTurma_input:
+        request_payload['idTurma'] = idTurma_input.get('value')
+
+    viewstate_input = form.find('input', attrs={'name': 'javax.faces.ViewState'})
+    if viewstate_input:
+        request_payload['javax.faces.ViewState'] = viewstate_input.get('value')
+
+    request_payload[form_name + ":turmaVirtual"] = form_name + ":turmaVirtual"
+    turmas_array.append(request_payload)
+
+print(len(turmas_array))
 
